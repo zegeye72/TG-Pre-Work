d77f47608af4d47a18b3478699dded7cd8ca2ac9
Zegeye Desta <zegeye_desta@yahoo.com>
2018-10-22 14:55:01 -0600

    Adding good scoping

--- Code-Academy-Exercises/Introduction-To-JavaScript/Scope/Practice-and-Good-Scoping.js
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Scope/Practice-and-Good-Scoping.js
@@ -1,7 +1,6 @@
 const logVisibleLightWaves = () => {
   let lightWaves = 'Moonlight';
 	let region = 'The Arctic';
-  // Add if statement here:
   if (region === 'The Arctic'){
     let lightWaves = 'Northern Lights';
     console.log(lightWaves);
