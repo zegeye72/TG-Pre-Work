5e0d3b4f4ccac4ffac5da6ede46b61b271588ac7
Zegeye Desta <zegeye_desta@yahoo.com>
2018-10-22 19:13:14 -0600

    Adding accessing element

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Arrays/Accessing-Elements.js
@@ -0,0 +1,5 @@
+const famousSayings = ['Fortune favors the brave.', 'A joke is a very serious thing.', 'Where there is love there is life.'];
+
+const listItem = famousSayings[0]
+
+console.log(famousSayings[3]);
